6103b3a0e397665be544dc41f6eee043242f6ea4
Sarah Martel <57870970+sarahmarie23@users.noreply.github.com>
2023-04-03 18:14:39 -0700

    Adjusted card UI specifications

--- Grid/app/src/main/java/com/example/grid/MainActivity.kt
+++ Grid/app/src/main/java/com/example/grid/MainActivity.kt
@@ -6,10 +6,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.MutatePriority
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
@@ -17,6 +22,7 @@ import androidx.compose.ui.res.integerResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.grid.model.Topic
@@ -38,13 +44,19 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
+/*
+@Composable
+fun GridApp() {
+    GridTheme {
+        GridList(topics = )
+    }
+}
+*/
 @SuppressLint("ResourceType")
 @Composable
 fun TopicItemCard(topic : Topic, modifier: Modifier = Modifier) {
-    Card(modifier = modifier
-        .padding(8.dp)
-        .fillMaxWidth()) {
+    Card(modifier = Modifier
+        .requiredHeight(68.dp)) {
         Row() {
             Image(
                 painter = painterResource(topic.imageResourceId),
@@ -52,28 +64,44 @@ fun TopicItemCard(topic : Topic, modifier: Modifier = Modifier) {
                 modifier = Modifier.size(68.dp)
             )
             Column(
-                modifier = Modifier.padding(16.dp)
+                modifier = Modifier
+                    .padding(start = 16.dp, top = 16.dp, end = 16.dp)
             ) {
                 Text(
                     text = stringResource(topic.titleResourceID),
-
-                    style = MaterialTheme.typography.h6
+                    style = typography.body2
                 )
-                Spacer(modifier = Modifier.padding(8.dp))
-                Row() {
-                    Image(painter = painterResource(R.drawable.ic_grain),
+                Row(
+                    verticalAlignment = CenterVertically,
+                    modifier = Modifier.padding(vertical = 8.dp),
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.ic_grain),
                         contentDescription = null,
-                        colorFilter = ColorFilter.tint(Color.Black))
+                        colorFilter = ColorFilter.tint(Color.Black),
+                        modifier = Modifier.padding(end = 8.dp),
 
-                    Text(
-                        text = (topic.courseResourceId.toString())
                     )
+                    Text(
+                        text = topic.courseResourceId.toString(),
+                        style = typography.caption
+                    )
+
                 }
             }
         }
     }
 }
+/*
+@Composable
+private fun TopicsList(topicList: List<Topic>, modifier: Modifier = Modifier) {
+    LazyColumn {
+        items(topicList) {topic -> TopicItemCard(topic)
 
+        }
+    }
+}
+*/
 @Preview(showBackground = true)
 @Composable
 fun TopicItemCardPreview() {
