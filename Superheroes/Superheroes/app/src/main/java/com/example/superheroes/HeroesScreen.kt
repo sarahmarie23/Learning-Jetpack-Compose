bbe2ce61415565eeb6464e496d58dc77146bc78c
Sarah Martel <57870970+sarahmarie23@users.noreply.github.com>
2023-04-21 19:53:06 -0700

    Status bar colors for dark mode

--- Superheroes/Superheroes/app/src/main/java/com/example/superheroes/HeroesScreen.kt
+++ Superheroes/Superheroes/app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -2,9 +2,13 @@ package com.example.superheroes
 
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,6 +18,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.superheroes.model.HeroesRepository.heroes
 import com.example.superheroes.model.Superhero
 import com.example.superheroes.ui.theme.Shapes
 import com.example.superheroes.ui.theme.SuperheroesTheme
@@ -23,7 +28,12 @@ fun SuperheroListItem(superhero: Superhero, modifier: Modifier = Modifier) {
     Card(
         shape = Shapes.medium,
         elevation = 2.dp,
-        modifier = Modifier
+        modifier = Modifier.padding(
+            start = 16.dp,
+            top = 8.dp,
+            end = 16.dp,
+            bottom = 8.dp
+        )
     ) {
         Row(
             horizontalArrangement = Arrangement.SpaceAround,
@@ -55,6 +65,24 @@ fun SuperheroListItem(superhero: Superhero, modifier: Modifier = Modifier) {
         }
     }
 }
+/*
+@Composable
+fun SuperheroApp() {
+    Scaffold(
+        content = {
+            it
+            LazyColumn(
+                modifier = Modifier.background(MaterialTheme.colors.background)
+            ) {
+
+                items(heroes) {
+                    SuperheroListItem(superhero = it)
+                }
+            }
+        }
+    )
+}
+*/
 @Preview("Light Theme")
 @Preview("Dark Theme", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
@@ -68,3 +96,18 @@ fun HeroPreview() {
         SuperheroListItem(superhero = hero)
     }
 }
+/*
+@Preview("Light Theme")
+@Preview("Dark Theme", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun SuperheroAppPreview() {
+    val hero = Superhero(
+        R.string.hero1,
+        R.string.description1,
+        R.drawable.android_superhero1
+    )
+    SuperheroesTheme {
+        SuperheroApp()
+    }
+}
+*/
