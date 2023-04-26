7ed7be648b64e3fcdeefedea1358035c6d2caf0e
Sarah Martel <57870970+sarahmarie23@users.noreply.github.com>
2023-04-25 19:46:56 -0700

    DisplayTips progress

--- ThirtyTips/app/src/main/java/com/example/thirtytips/DisplayTips.kt
+++ ThirtyTips/app/src/main/java/com/example/thirtytips/DisplayTips.kt
@@ -6,10 +6,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -54,10 +58,34 @@ fun TipCard(tip: Tip, modifier: Modifier = Modifier) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DisplayTips() {
+    Scaffold(
+        topBar = {},
+        content = {
+            it
+            LazyColumn() {
+                items(tips) {
+                    TipCard(tip = it)
+                }
+            }
+        }
+    )
+
+
+}
 
 @Preview
 @Composable
 fun TipCardPreview() {
     TipCard(tips[0])
+
+}
+
+@Preview
+@Composable
+fun DisplayTipsPreview() {
+    DisplayTips()
 
 }
