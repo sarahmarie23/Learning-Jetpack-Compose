8e5bfd45fb9f06ab413409a7f38f59884bea30f5
Sarah Martel <57870970+sarahmarie23@users.noreply.github.com>
2023-06-13 10:47:31 -0700

    Add screen enum

--- LunchTray/app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
+++ LunchTray/app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -15,6 +15,7 @@
  */
 package com.example.lunchtray
 
+import androidx.annotation.StringRes
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -22,7 +23,13 @@ import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.lunchtray.ui.OrderViewModel
 
-// TODO: Screen enum
+enum class LunchTrayScreen(@StringRes val title: Int) {
+    Start(title = R.string.app_name),
+    Entree(title = R.string.choose_entree),
+    Side(title = R.string.choose_side_dish),
+    Accompaniment(title = R.string.choose_accompaniment),
+    Checkout(title = R.string.order_checkout)
+}
 
 // TODO: AppBar
 
